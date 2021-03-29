dbf4e20d537321c59679432921232473629362cb
Suryakumar RS <81559115+SuryakumarRS@users.noreply.github.com>
2021-03-29 15:06:29 +0530

    Update AppointDetails.java

--- AppointDetails.java
+++ AppointDetails.java
@@ -4,10 +4,10 @@ public class AppointDetails {
 		
 	public static void main(String args[]){  
 	
-		print();
+		solve();
 	}	
 		
-	static void print() {
+	static void solve() {
 		try{  
 			Class.forName("com.mysql.cj.jdbc.Driver");  
 			Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/clinicmanagement","root","tiger");  				  
